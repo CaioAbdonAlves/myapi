ddcc5c8218d8edfc3bf2304a094d62b5d9c46845
Caio Abdon <a.caio@cicclo.com>
2024-02-06 01:40:41 -0300

    Add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from '@shared/errors/AppError'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
+  throw new AppError('Acesso Negado', 401)
   return response.json({ body: { message: 'Opa safado' } })
 })
 
