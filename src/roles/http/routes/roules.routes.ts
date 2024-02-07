f9bad0fd78f73d49a02987aef966fc8d3de3e08c
Caio Abdon <a.caio@cicclo.com>
2024-02-06 21:59:24 -0300

    Create findByName method

--- src/roles/http/routes/roules.routes.ts
+++ src/roles/http/routes/roules.routes.ts
@@ -7,9 +7,21 @@ const rolesRepository = new RoleRepository()
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body
 
+  const roleAlreadyExists = rolesRepository.findByName(name)
+
+  if (roleAlreadyExists) {
+    return response.status(400).json({ error: 'Role already exists' })
+  }
+
   const role = rolesRepository.create({ name })
 
   return response.status(201).json({ body: role })
+})
+
+rolesRouter.get('/', (request, response) => {
+  const roles = rolesRepository.findAll()
+
+  return response.status(200).json({ body: roles })
 })
 
 export { rolesRouter }
