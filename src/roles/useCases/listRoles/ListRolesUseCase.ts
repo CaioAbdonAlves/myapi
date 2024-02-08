9809001f2e3f16583626a70fa1b925b78dcb4ef4
Caio Abdon <a.caio@cicclo.com>
2024-02-08 15:29:58 -0300

    Refactor useCases

--- src/roles/useCases/listRoles/ListRolesUseCase.ts
+++ src/roles/useCases/listRoles/ListRolesUseCase.ts
@@ -1,10 +1,22 @@
-import { Role } from '@roles/entities/Role'
-import { RoleRepository } from '@roles/repositories/RoleRepository'
+import {
+  RoleRepository,
+  RolesPaginateProperties,
+} from '@roles/repositories/RoleRepository'
+
+type ListRolesUseCaseParams = {
+  page: number
+  limit: number
+}
 
 export class ListRoulesUseCase {
   constructor(private rolesRepository: RoleRepository) {}
 
-  execute(): Role[] {
-    return this.rolesRepository.findAll()
+  async execute({
+    page,
+    limit,
+  }: ListRolesUseCaseParams): Promise<RolesPaginateProperties> {
+    const take = limit
+    const skip = (Number(page) - 1) * take
+    return this.rolesRepository.findAll({ page, skip, take })
   }
 }
