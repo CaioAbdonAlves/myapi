24cc4f66fa10d3dbe2ebf30ff9b3b02ae304ddda
Caio Abdon <a.caio@cicclo.com>
2024-02-07 21:22:03 -0300

    Refactory role entity

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -1,8 +1,15 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
+@Entity('roles')
 export class Role {
+  @PrimaryColumn()
   public id?: string
+
+  @Column()
   public name: string
+
+  @CreateDateColumn()
   public created_at: Date
 
   constructor() {
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1707347931057 } from './migrations/1707347931057-CreateRolesTable'
+import { Role } from '@roles/entities/Role'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1707347931057],
 })
