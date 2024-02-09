839c55f32597f265dbfb7d462b329ffbfc318d2b
Caio Abdon <a.caio@cicclo.com>
2024-02-08 21:15:30 -0300

    Create usecase deleterole

---
+++ src/roles/useCases/deleteRole/DeleteRoleUseCase.ts
@@ -0,0 +1,20 @@
+import { RoleRepository } from '@roles/repositories/RoleRepository'
+import { AppError } from '@shared/errors/AppError'
+
+type DeleteRoleParams = {
+  id: string
+}
+
+export class DeleteRoleUseCase {
+  constructor(private rolesRepository: RoleRepository) {}
+
+  async execute({ id }: DeleteRoleParams): Promise<void> {
+    const role = await this.rolesRepository.findById(id)
+
+    if (!role) {
+      throw new AppError('Role not found', 404)
+    }
+
+    await this.rolesRepository.delete(role)
+  }
+}
