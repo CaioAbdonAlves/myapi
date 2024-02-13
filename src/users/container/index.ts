c6f581f205f0d9d254e17360355f2b4a122d224b
Caio Abdon <a.caio@cicclo.com>
2024-02-13 20:02:23 -0300

    Configure container and route for ListUsers

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -2,6 +2,7 @@ import { container } from 'tsyringe'
 import { UsersRepository } from '@users/repositories/UsersRepository'
 import { IUsersRepository } from '@users/repositories/IUsersRepository'
 import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
+import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
@@ -9,3 +10,4 @@ container.registerSingleton<IUsersRepository>(
 )
 
 container.registerSingleton('CreateUserController', CreateUserController)
+container.registerSingleton('ListUsersController', ListUsersController)
