a7aea6bf984a269ca34a90314934957daebd7c2f
Caio Abdon <a.caio@cicclo.com>
2024-02-13 16:36:21 -0300

    Add CreateUser route

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,8 +1,11 @@
 import { container } from 'tsyringe'
 import { UsersRepository } from '@users/repositories/UsersRepository'
 import { IUsersRepository } from '@users/repositories/IUsersRepository'
+import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 )
+
+container.registerSingleton('CreateUserController', CreateUserController)
