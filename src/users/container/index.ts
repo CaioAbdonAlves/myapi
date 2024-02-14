85aa97741121d14e90a635a2f7529b2c1f56d226
Caio Abdon <a.caio@cicclo.com>
2024-02-14 01:04:18 -0300

    Add CreateLoginController and Route

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -3,6 +3,7 @@ import { UsersRepository } from '@users/repositories/UsersRepository'
 import { IUsersRepository } from '@users/repositories/IUsersRepository'
 import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
 import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
+import { CreateLoginController } from '@users/useCases/createLogin/CreateLoginController'
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
@@ -11,3 +12,4 @@ container.registerSingleton<IUsersRepository>(
 
 container.registerSingleton('CreateUserController', CreateUserController)
 container.registerSingleton('ListUsersController', ListUsersController)
+container.registerSingleton('CreateLoginController', CreateLoginController)
