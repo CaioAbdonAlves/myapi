3d6c6b270331fa48a7ef7611bcf6bf31d8f949cd
Caio Abdon <a.caio@cicclo.com>
2024-02-14 21:24:47 -0300

    Add user avatar_url property

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,17 +1,20 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import path from 'node:path'
 import { errors } from 'celebrate'
 import { routes } from './routes'
 import { AppError } from '@shared/errors/AppError'
 import swaggerUi from 'swagger-ui-express'
 import swaggerFile from '../../swagger.json'
 import '@shared/container'
+import uploadConfig from '@config/upload'
 
 const app = express()
 app.use(cors())
 
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(routes)
